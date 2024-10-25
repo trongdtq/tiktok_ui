16e7e2ded1e818d64e91f706cf8da0ce3efe6058
T-Quyen <dangtrongquyen1@gmail.com>
2024-10-25 17:26:36 +0700

    Update base dự án tiktok-ui

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+// Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
+import Upload from '~/pages/Upload';
+
+// Public Routes
+const publicRoutes = [
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/profile', component: Profile },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+  { path: '/search', component: Search, layout: null },
+];
+
+// Private Routes
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
